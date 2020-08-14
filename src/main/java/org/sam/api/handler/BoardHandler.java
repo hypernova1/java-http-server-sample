e17b9f796e0c9bae2426f906083c9f50c1759b2e
hypernova1 <hypemova@gmail.com>
2020-08-14 14:38:12 +0900

    패키지 변경

--- src/main/java/org/sam/api/handler/BoardHandler.java
+++ src/main/java/org/sam/api/handler/BoardHandler.java
@@ -2,11 +2,14 @@ package org.sam.api.handler;
 
 import org.sam.api.domain.Post;
 import org.sam.api.service.BoardService;
-import org.sam.server.annotation.handle.*;
+import org.sam.server.annotation.component.Handler;
+import org.sam.server.annotation.handle.GetHandle;
+import org.sam.server.annotation.handle.JsonRequest;
+import org.sam.server.annotation.handle.PostHandle;
+import org.sam.server.annotation.handle.RestApi;
 import org.sam.server.http.ResponseEntity;
 
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Created by melchor
@@ -25,8 +28,8 @@ public class BoardHandler {
     @RestApi
     @GetHandle
     public ResponseEntity<?> getPostList() {
-        List<Post> postList = boardService.getBoardList();
-        return ResponseEntity.ok(postList);
+//        List<Post> postList = boardService.getBoardList();
+        return ResponseEntity.ok(null);
     }
 
     @RestApi
